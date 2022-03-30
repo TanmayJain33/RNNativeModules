56a1d9ece3a822b7af40b445d2261ba57738e028
TanmayJain33 <tanmay.jain@crownstack.com>
2022-03-30 12:59:16 +0530

    Add Android Native Module

---
+++ App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {NativeModules, Button} from 'react-native';
+
+const {CalendarModule} = NativeModules;
+
+export default function App() {
+  const onPress = () => {
+    console.log('We will invoke the native module here!');
+    CalendarModule.createCalendarEvent('Tanmay Jain', 'Gurugram');
+  };
+
+  return (
+    <Button
+      title="Click to invoke your native module"
+      color="#841584"
+      onPress={onPress}
+    />
+  );
+}
